7580e2b944f4a32aa48c91b5ede60bca20623bf7
Kamarali <kdapple.mi@gmail.com>
2017-10-21 12:24:44 +0530

    dded new tab and some UI stuff

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,6 +6,8 @@ import { MyApp } from './app.component';
 import { AboutPage } from '../pages/about/about';
 import { ContactPage } from '../pages/contact/contact';
 import { HomePage } from '../pages/home/home';
+import { YouPage } from '../pages/you/you';
+import { BasicKnowledgePage } from '../pages/basicknowledge/basicknowledge';
 import { TabsPage } from '../pages/tabs/tabs';
 
 import { StatusBar } from '@ionic-native/status-bar';
@@ -17,6 +19,8 @@ import { SplashScreen } from '@ionic-native/splash-screen';
     AboutPage,
     ContactPage,
     HomePage,
+    YouPage,
+    BasicKnowledgePage,
     TabsPage
   ],
   imports: [
@@ -29,6 +33,8 @@ import { SplashScreen } from '@ionic-native/splash-screen';
     AboutPage,
     ContactPage,
     HomePage,
+    YouPage,
+    BasicKnowledgePage,
     TabsPage
   ],
   providers: [
